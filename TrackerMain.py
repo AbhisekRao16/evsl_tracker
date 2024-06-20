f435c14aaf8ab0971dd98b6c6aade305a16c5bdf
AbhisekRao16 <abhisekrao1608@gmail.com>
2024-06-20 13:49:03 +0530

    changed after meet

--- TrackerMain.py
+++ TrackerMain.py
@@ -8,7 +8,7 @@ t = time.time()
 
 
 #path of the dataset
-sensor_data = r"C:\Users\lenovo.LALITH\Desktop\sample-evonith\VariablesAndSensorsData_2hours.txt"
+sensor_data = r"C:\Users\lenovo.LALITH\Desktop\voptimAI\sample-evonith\VariablesAndSensorsData_2hours.txt"
 
 # Create DataCleaning object :data_cleaner
 data_cleaner = DataCleaning(sensor_data)
@@ -22,3 +22,4 @@ data_tracker.print_sensor_states(dataframe1)
 #end time
 s = time.time()
 print(s - t)
+#TODO: cpython in place of python
