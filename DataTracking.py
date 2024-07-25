fc2e7caeb8361dceecd385318b7d7189b63fe742
AbhisekRao16 <abhisekrao1608@gmail.com>
2024-07-25 12:16:34 +0530

    new file: check_abhi

--- DataTracking.py
+++ DataTracking.py
@@ -146,5 +146,5 @@ class DataTracking:
         # self.df.fillna(value=pd.NaT, inplace=True)
         # self.df.dropna(subset=self.df.columns[1:], how='all', inplace=True)
         print(self.df)
-        self.df.to_csv(r"C:\Users\Abhishek\Desktop\evsl_tracker\evsl_tracker_output_excel\check.csv")
+        self.df.to_csv(r"C:\Users\Abhishek\Desktop\evsl_tracker\evsl_tracker_output_excel\check_abhi.csv")
         return self.df
